7cedea518d8ce47edd4669c0d7c7b4db5b0ddb55
Sebastian Straube <github@straube.ch>
2017-03-26 22:33:52 +0200

    patch

--- cc-host-install/04_install-pip.sh
+++ cc-host-install/04_install-pip.sh
@@ -6,8 +6,7 @@ function pause(){
 
 ############### INSTALL PIP ###################
 #http://stackoverflow.com/questions/22073516/failed-to-install-python-cryptography-package-with-pip-and-setup-py
-sudo yum -y install gcc libffi-devel python-devel openssl-devel
-sudo yum -y install python-pip
+sudo yum -y install gcc libffi-devel python-devel openssl-devel python-pip
 sudo easy_install pip
 sudo pip install --upgrade pip
 sudo pip install cryptography
